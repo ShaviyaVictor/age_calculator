a60b4afdacbfcb8a65b766aa52bc1e8d6ca95eca
shaviyavictor <shaviyavictor@gmail.com>
2022-05-24 14:34:19 +0300

    Create variables for getting the current time

--- assets/logic.js
+++ assets/logic.js
@@ -3,4 +3,9 @@ function age() {
   var d1 = document.getElementById('date').value;
   var m1 = document.getElementById('month').value;
   var y1 = document.getElementById('year').value;
+  // variables for getting the current time
+  var date = new Date();
+  var d2 = date.getDate();
+  var m2 = 1 + date.getMonth();
+  var y2 = date.getFullYear();
 }
