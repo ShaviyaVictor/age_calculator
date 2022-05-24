616a26a508361b4f334365a9c453caea75b95a04
shaviyavictor <shaviyavictor@gmail.com>
2022-05-24 14:26:58 +0300

    Create variables for getting the input field values

---
+++ assets/logic.js
@@ -0,0 +1,6 @@
+function age() {
+  // variables for getting the input field values
+  var d1 = document.getElementById('date').value;
+  var m1 = document.getElementById('month').value;
+  var y1 = document.getElementById('year').value;
+}
